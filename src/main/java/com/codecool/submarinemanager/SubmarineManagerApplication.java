dcd7ae214d34f893eef9849ef62e63bbd9a28368
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-15 14:44:48 +0100

    Add SubmarineManagerApplication class

---
+++ src/main/java/com/codecool/submarinemanager/SubmarineManagerApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.submarinemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmarineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmarineManagerApplication.class, args);
+	}
+}
