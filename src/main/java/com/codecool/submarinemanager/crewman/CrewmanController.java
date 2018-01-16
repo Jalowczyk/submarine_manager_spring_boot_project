a5e2a67eb8abade404e3e15099374fe39c97cb1e
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-16 13:46:56 +0100

    Add constructor

--- src/main/java/com/codecool/submarinemanager/crewman/CrewmanController.java
+++ src/main/java/com/codecool/submarinemanager/crewman/CrewmanController.java
@@ -1,4 +1,15 @@
 package com.codecool.submarinemanager.crewman;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/crewmen")
 public class CrewmanController {
+
+    private CrewmanService crewmanService;
+
+    public CrewmanController(CrewmanService crewmanService) {
+        this.crewmanService = crewmanService;
+    }
 }
