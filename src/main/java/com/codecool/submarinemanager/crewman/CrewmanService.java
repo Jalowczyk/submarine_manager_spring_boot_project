d117ee44eba8f1bd4b70611ad8325a9e41438c82
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-16 13:06:26 +0100

    Change package

--- src/main/java/com/codecool/submarinemanager/crewman/CrewmanService.java
+++ src/main/java/com/codecool/submarinemanager/crewman/CrewmanService.java
@@ -1,4 +1,21 @@
 package com.codecool.submarinemanager.crewman;
 
+import com.codecool.submarinemanager.crewman.Crewman;
+import com.codecool.submarinemanager.crewman.CrewmanRepository;
+
 public class CrewmanService {
+
+    private CrewmanRepository crewmanRepository;
+
+    public CrewmanService(CrewmanRepository crewmanRepository) {
+        this.crewmanRepository = crewmanRepository;
+    }
+
+    public Iterable<Crewman> findAllCrewmen() {
+        return crewmanRepository.findAll();
+    }
+
+    public Crewman findById(Integer id) {
+        return crewmanRepository.findOne(id);
+    }
 }
