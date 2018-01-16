16c2fee5eef5f3ada19b0c10b4cf75642cd917d7
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-16 12:58:11 +0100

    Create imports

--- src/main/java/com/codecool/submarinemanager/crewman/CrewmanRepository.java
+++ src/main/java/com/codecool/submarinemanager/crewman/CrewmanRepository.java
@@ -1,10 +1,7 @@
 package com.codecool.submarinemanager.crewman;
 
-import com.codecool.submarinemanager.submarine.Submarine;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 @Repository
 public interface CrewmanRepository extends CrudRepository<Crewman, Integer> {
--- src/main/java/com/codecool/submarinemanager/submarine/SubmarineRepository.java
+++ src/main/java/com/codecool/submarinemanager/submarine/SubmarineRepository.java
@@ -3,8 +3,6 @@ package com.codecool.submarinemanager.submarine;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface SubmarineRepository extends CrudRepository<Submarine, Integer> {
 }
