f7c6cae5cf78f846bd3f65b4f3bfb49c2ee8f411
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-16 14:19:33 +0100

    Add SubmarineController constructor

--- src/main/java/com/codecool/submarinemanager/submarine/SubmarineController.java
+++ src/main/java/com/codecool/submarinemanager/submarine/SubmarineController.java
@@ -1,4 +1,15 @@
 package com.codecool.submarinemanager.submarine;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/submarines")
 public class SubmarineController {
+
+    private SubmarineService submarineService;
+
+    public SubmarineController(SubmarineService submarineService) {
+        this.submarineService = submarineService;
+    }
 }
