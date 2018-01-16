7ef7e7206528546dd50c560846584c2d2e6344b3
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-16 13:00:41 +0100

    Add contructor in CrewmanService

--- src/main/java/com/codecool/submarinemanager/submarine/CrewmanService.java
+++ src/main/java/com/codecool/submarinemanager/submarine/CrewmanService.java
@@ -1,4 +1,14 @@
 package com.codecool.submarinemanager.submarine;
 
+import com.codecool.submarinemanager.crewman.CrewmanRepository;
+
 public class CrewmanService {
+
+    private CrewmanRepository crewmanRepository;
+
+    public CrewmanService(CrewmanRepository crewmanRepository) {
+        this.crewmanRepository = crewmanRepository;
+    }
+
+
 }
