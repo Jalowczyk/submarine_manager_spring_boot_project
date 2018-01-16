e8055f4ef8e38db54d2bad51afb09099d9785166
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-16 11:30:44 +0100

    Add annotations in Crewman

--- src/main/java/com/codecool/submarinemanager/crewman/Crewman.java
+++ src/main/java/com/codecool/submarinemanager/crewman/Crewman.java
@@ -1,18 +1,32 @@
 package com.codecool.submarinemanager.crewman;
 
+import com.codecool.submarinemanager.submarine.Submarine;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Crewman {
 
+    @Id
+    @GeneratedValue
     private Integer id;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
     private String rank;
+    @ManyToOne
+    private Submarine submarine;
 
-    public Crewman(Integer id, String firstName, String lastName, String rank) {
+    public Crewman(Integer id, String firstName, String lastName, String rank, Submarine submarine) {
 
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.rank = rank;
+        this.submarine = submarine;
     }
 
     public Integer getId() {
@@ -45,5 +59,13 @@ public class Crewman {
 
     public void setRank(String rank) {
         this.rank = rank;
+    }
+
+    public Submarine getSubmarine() {
+        return submarine;
+    }
+
+    public void setSubmarine(Submarine submarine) {
+        this.submarine = submarine;
     }
 }
