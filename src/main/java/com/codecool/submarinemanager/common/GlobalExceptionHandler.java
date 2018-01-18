a5202e93499c49dc5269affef18872ef469623a4
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-18 16:01:23 +0100

    Add logger to GlobalExceptionHandler

--- src/main/java/com/codecool/submarinemanager/common/GlobalExceptionHandler.java
+++ src/main/java/com/codecool/submarinemanager/common/GlobalExceptionHandler.java
@@ -2,9 +2,7 @@ package com.codecool.submarinemanager.common;
 
 import com.codecool.submarinemanager.exception.IdDoesNotExistException;
 import com.codecool.submarinemanager.response.ErrorResponse;
-import javassist.NotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -22,42 +20,49 @@ public class GlobalExceptionHandler {
    @ExceptionHandler(ConstraintViolationException.class)
    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
        ErrorResponse err = new ErrorResponse("database constrains failed", ex.getMessage());
+       AppLogger.getLogger().error(AppLogger.getMarker(), err.getCode());
        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
    }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        ErrorResponse err = new ErrorResponse("database integrity failed", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+       ErrorResponse err = new ErrorResponse("database integrity failed", ex.getMessage());
+       AppLogger.getLogger().error(AppLogger.getMarker(), err.getCode());
+       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(IdDoesNotExistException.class)
     public ResponseEntity<ErrorResponse> handleIdDoesNotExistException(IdDoesNotExistException ex) {
         ErrorResponse err = new ErrorResponse("id doesn't exist or resource was archived", ex.getMessage());
+        AppLogger.getLogger().error(AppLogger.getMarker(), err.getCode());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
         ErrorResponse err = new ErrorResponse("this request method is not supported", ex.getMessage());
+        AppLogger.getLogger().error(AppLogger.getMarker(), err.getCode());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException ex) {
         ErrorResponse err = new ErrorResponse("element not found", ex.getMessage());
+        AppLogger.getLogger().error(AppLogger.getMarker(), err.getCode());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
         ErrorResponse err = new ErrorResponse("wrong data format", ex.getMessage());
+        AppLogger.getLogger().error(AppLogger.getMarker(), err.getCode());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
         ErrorResponse err = new ErrorResponse("wrong method's argument", ex.getMessage());
+        AppLogger.getLogger().error(AppLogger.getMarker(), err.getCode());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
