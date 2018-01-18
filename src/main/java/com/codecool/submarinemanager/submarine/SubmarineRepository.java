e325ad4ad8c45720959abcafa81f6d72a0537ca5
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-18 10:28:43 +0100

    Add custom methods

--- src/main/java/com/codecool/submarinemanager/submarine/SubmarineRepository.java
+++ src/main/java/com/codecool/submarinemanager/submarine/SubmarineRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubmarineRepository extends CrudRepository<Submarine, Integer> {
+
+    public Iterable<Submarine> findAllByArchivedFalse();
+    public Submarine findSubmarineByArchivedFalseAndId(Integer id);
 }
